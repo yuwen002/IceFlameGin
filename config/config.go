13203fc357112b32fb69e6b2730067aaae77b30a
yuwen002 <yuwen002@163.com>
2024-02-01 15:53:40 +0800

    编写Models

--- config/config.go
+++ config/config.go
@@ -1,1 +1,17 @@
 package config
+
+type DatabaseConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	DBName   string
+}
+
+var DBConfig = DatabaseConfig{
+	Host:     "localhost",
+	Port:     3306,
+	Username: "root",
+	Password: "password",
+	DBName:   "myapp",
+}
