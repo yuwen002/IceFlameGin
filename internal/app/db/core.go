bc55cc5a653d220c2030a57ff91e6f9ead605b72
yuwen002 <yuwen002@163.com>
2024-02-05 00:50:25 +0800

    增加验证器

--- internal/app/db/core.go
+++ internal/app/db/core.go
@@ -6,6 +6,7 @@ import "gorm.io/gorm"
 // @Description: 数据库核心类接口
 type DatabaseCore interface {
 	Insert(data interface{}) error
+	InsertAndGetID(data interface{}) (uint, error)
 	Update(condition string, data interface{}) error
 	Query(condition string) ([]interface{}, error)
 }
@@ -29,8 +30,56 @@ func (g *GormCore) Insert(data interface{}) error {
 	return g.db.Create(data).Error
 }
 
+// InsertAndGetID
+//
+// @Title InsertAndGetID
+// @Description: 插入数据并返回 ID
+// @Author liuxingyu
+// @Date 2024-02-03 23:11:48
+// @receiver g
+// @param data
+// @return uint
+// @return error
+func (g *GormCore) InsertAndGetID(data interface{}) (uint, error) {
+	result := g.db.Create(data)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	var id uint
+	if err := g.db.Model(data).Pluck("id", &id).Error; err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+// Update
+//
+// @Title Update
+// @Description: 条件修改数据
+// @Author liuxingyu
+// @Date 2024-02-03 23:26:09
+// @receiver g
+// @param condition
+// @param data
+// @return error
 func (g *GormCore) Update(condition string, data interface{}) error {
 	return g.db.Where(condition).Updates(data).Error
+}
+
+// UpdateByID
+//
+// @Title UpdateByID
+// @Description: 按ID修改数据
+// @Author liuxingyu
+// @Date 2024-02-03 23:27:38
+// @receiver g
+// @param id
+// @param data
+// @return error
+func (g *GormCore) UpdateByID(id uint, data interface{}) error {
+	return g.db.Model(data).Where("id = ?", id).Updates(data).Error
 }
 
 func (g *GormCore) Query(condition string) ([]interface{}, error) {
