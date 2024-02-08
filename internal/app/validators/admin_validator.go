83611c6beb2fb204e99732bbd702431485514f13
yuwen002 <yuwen002@163.com>
2024-02-09 03:57:38 +0800

    修改validator form 验证BUG 增加注册页面做逻辑，增加处理注册页面处理页 修改系统类validator，更换自定义输出

--- internal/app/validators/admin_validator.go
+++ internal/app/validators/admin_validator.go
@@ -8,7 +8,7 @@ package validators
 type AdminLoginForm struct {
 	Tel      string `form:"tel" binding:"required,min=5,max=32" msg:"用户名不能为空|用户名长度不能小于5个字符|用户名长度不能超过32个字符"`
 	Password string `form:"password" binding:"required,min=5,max=32" msg:"密码不能为空|密码长度不能小于5个字符|密码长度不能超过32个字符"`
-	Remember string `form:"remember"`
+	Remember bool   `form:"remember"`
 }
 
 //	AdminRegisterForm
