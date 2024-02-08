7a92fe802a6ee7a5e06bdf71bae5eaba616a6a05
yuwen002 <yuwen002@163.com>
2024-02-09 02:43:18 +0800

    变更Config

--- config/config.go
+++ config/config.go
@@ -12,6 +12,6 @@ var DBConfig = DatabaseConfig{
 	Host:     "82.157.248.230",
 	Port:     3306,
 	Username: "go_test",
-	Password: "XLx8EhRTEkF5fLLr111111",
+	Password: "go_test",
 	DBName:   "go_test",
 }
