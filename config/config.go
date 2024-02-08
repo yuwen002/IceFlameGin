d17752c92ee287e553a92ac921e0220a881f4cb8
yuwen002 <yuwen002@163.com>
2024-02-09 01:26:55 +0800

    修改validator form 验证BUG 增加注册页面做逻辑，增加处理注册页面处理页

--- config/config.go
+++ config/config.go
@@ -12,6 +12,6 @@ var DBConfig = DatabaseConfig{
 	Host:     "82.157.248.230",
 	Port:     3306,
 	Username: "go_test",
-	Password: "XLx8EhRTEkF5fLLr",
+	Password: "XLx8EhRTEkF5fLLr111111",
 	DBName:   "go_test",
 }
