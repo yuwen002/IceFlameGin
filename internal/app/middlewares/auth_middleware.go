3453f523e70d556d82efad25b453cecb1d6a5db5
yuwen002 <yuwen002@163.com>
2024-03-20 10:42:35 +0800

    添加中间件禁用缓存

--- internal/app/middlewares/auth_middleware.go
+++ internal/app/middlewares/auth_middleware.go
@@ -10,6 +10,9 @@ import (
 
 func MasterAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 设置响应头，禁用缓存
+		c.Writer.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+
 		token := system.GetSession(c, "ice_flame_master")
 		if token == nil {
 			system.RedirectGet(c, paths.AdminRoot)
